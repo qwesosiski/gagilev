705a3b29dfa9a4e31d503cd18784b778bb400ca8
Студент группы IS <is@sielom.local>
2025-09-26 09:44:39 +0500

    project1.2

--- hello/models.py
+++ hello/models.py
@@ -6,8 +6,22 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(verbose_name='Имя автора', max_length=20)
     surname = models.CharField("Фамилия",max_length=25)
-    bitrhday = models.DateField("Биография")
+    bitrhday = models.DateField("Биография", default='1999-06-02')
     bio = models.TextField("Описание")
+
+    class Meta: 
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+        ordering = ['surname','name']
+        indexes = [
+            models.Index(fields=['surname'])
+        ]
+        constraints = [
+            models.UniqueConstraint (
+                fields = ['surname','bio'],
+                name = 'unique_surname_bio'
+            ),
+        ]
 
 class Publisher(models.Model):
     name = models.CharField("Название",unique=True)
