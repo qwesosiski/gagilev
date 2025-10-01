430efbef028720132f1a2610c725aec9ef94e963
Студент группы IS <is@sielom.local>
2025-10-01 09:21:47 +0500

    12342

--- hello/models.py
+++ hello/models.py
@@ -4,39 +4,21 @@ from django.db import models
 class Subscription(models.Model):
     title = models.CharField("Описание", max_length=50)
     price = models.DecimalField ("Цена")
-    expiry_date = models.DateField("Срок истечения")
+    validity_period = models.DateField("Срок истечения")
     
     class Meta:
         verbose_name = "Подписка" 
         verbose_name_plural = "Подписки"
         subscription = ["price"]
         indexes = [
-            models.Index(fields=["expiry_date"]), 
+            models.Index(fields=["validity_period"]), 
             models.Index(fields=["price"]),
             models.Index(fields=["title"])
         ]
-
-        constraints = [
-            models.UniqueConstraint(
-                fields = ["price", "title"],
-                condition = models.Q(desc = "expiry_date"),
-                name = "unique_surname_bio"
-            ),
-            ]
         
     def str(self):
         return f"{self.surname} {self.name}"
 
-class User(models.Model):
-    title = models.CharField("Должность", unique=True)
-    class Meta:
-        verbose_name = "Должность"
-        verbose_name_plural = "Должности"
-        indexes = [
-            models.Index(fields=["Должность"])
-        ]
-    def str(self):
-        return f"{self.surname} {self.name}"
     
 class User(models.Model):
     username = models.CharField("Имя пользователя", max_length=50)
@@ -50,41 +32,65 @@ class User(models.Model):
         verbose_name_plural = "Пользователи"
         ordering = ["username"]
         indexes = [
-            models.Index(fields=["Имя"]), # для быстрого поиска
+            models.Index(fields=["Имя пользователя"]), # для быстрого поиска
             models.Index(fields=["Номер телефона"]),
             models.Index(fields=["Почта"])
         ]
 
-        constraints = [
-            models.UniqueConstraint(
-                fields = ["surname", "bio"],
-                condition = models.Q(desc = "Жив"),
-                name = "unique_surname_bio"
-            ),
-            ]
     def str(self):
         return f"{self.surname} {self.name}"
     
 
-class Chats(models.Model):
-    UserId = models.CharField ("Имя пользователя")
-    MessageID = models.CharField ("Сообщения")
+class Chat (models.Model):
+    User_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField("Описание")
+    data = models.CharField("Данные")
+    name = models.CharField("Имя")
 
     class Meta:
         verbose_name = "Чат"
         verbose_name_plural = "Чаты"
-        ordering = ["start_time"]
+        ordering = ["name"]
         indexes = [
-            models.Index(fields=["start_time"]), 
-            models.Index(fields=["end_time"])
+            models.Index(fields=["name"]), 
         ]
 
-        constraints = [
-            models.UniqueConstraint(
-                fields = ["surname", "bio"],
-                condition = models.Q(desc = "Жив"),
-                name = "unique_surname_bio"
-            ),
-            ]
+    def str(self):
+        return f"{self.surname} {self.name}"
+    
+class Message(models.Model):
+    User_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    sending_time = models.CharField("Время отправки")
+    Chat_id = models.ForeignKey(Chat, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Сообщение"
+        verbose_name_plural = "Сообщения"
+        ordering = ["sednding_time"]
+        indexes = [
+            models.Index(fields=["sending_time"]),
+            models.Index(fields=["Chat_id"]),
+            models.Index(fields=["User_id"]), 
+        ]
+
+    def str(self):
+        return f"{self.surname} {self.name}"
+    
+
+
+class Channel(models.Model):
+    User_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField("Описание")
+    data = models.CharField("Данные")
+    name = models.CharField("Имя")
+
+    class Meta:
+        verbose_name = "Канал"
+        verbose_name_plural = "Каналы"
+        ordering = ["name"]
+        indexes = [
+            models.Index(fields=["name"]), 
+        ]
+
     def str(self):
         return f"{self.surname} {self.name}"
